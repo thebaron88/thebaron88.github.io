61fbdae8387c89b8a8f432458f624e55c5759db8
Mark Fairbairn <mark@fairbairn.xyz>
2020-07-28 11:27:54 +0100

    And instructions on how to flash

---
+++ flash_lineage.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+PATH=%PATH%;"%~dp0\platform-tools"
+rem 7z x platform-tools_r26.0.2-windows.zip
+
+adb reboot bootloader
+fastboot boot twrp-3.4.0-0-mako.img
+
+echo Press any key when recovery has booted
+pause >nul
+
+adb shell twrp wipe cache
+adb shell twrp wipe dalvik
+adb shell twrp wipe data
+adb shell mkdir /cache/recovery
+adb shell twrp sideload
+adb sideload lineage-17.1-20200713-UNOFFICIAL-mako.zip
+adb reboot system
+
+echo Press any key when adb is enabled on the device
+pause >nul
+
+adb install sensors.apk
+
+echo Press any key to exit...
+pause >nul
+exit
