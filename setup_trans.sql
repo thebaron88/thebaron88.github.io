5845fd957c9b775fa0bca10476e97474d236464b
Mark Fairbairn <mark.fairbairn@etas.com>
2020-08-01 15:24:30 +0100

    proposed transaction database

--- setup_trans.sql
+++ setup_trans.sql
@@ -2,6 +2,7 @@ create table if not exists transactions
 (
 	can INTEGER not null,
 	price INTEGER not null,
-	transid STRING not null,
+	description STRING not null,
+	status INTEGER not null,
 	timestamp TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP
 );
