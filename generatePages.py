536073d4ffa48506ece41d9cc8fa400fe1627182
Mark Fairbairn <mark@fairbairn.xyz>
2020-08-04 12:04:49 +0100

    All cans added

--- generatePages.py
+++ generatePages.py
@@ -3,14 +3,25 @@ import sqlite3
 
 def setup_database():
     images = {
-        "Dr Pepper": "https://ui.assets-asda.com/dm/asdagroceries/5449000060501_T1",
+        "Full Dr Pepper": "https://ui.assets-asda.com/dm/asdagroceries/5449000060501_T1",
         "Coke Zero": "https://ui.assets-asda.com/dm/asdagroceries/5449000031341_T1",
         "Full Coke": "https://ui.assets-asda.com/dm/asdagroceries/5449000051547_T1",
+        "Full Pepsi": "TODO",
+        "Fanta Fruit Twist": "TODO",
+        "Sprite": "TODO",
+        "Pepsi Max": "TODO",
+        "Lilt": "TODO",
+        "Emerge Energy Drink": "TODO",
     }
     batches = {
-        "0": {"name": 1, "price": 0.55, "expiry": "2021-06-30"},
-        "1": {"name": 2, "price": 0.50, "expiry": "2020-06-30"},
-        "2": {"name": 3, "price": 0.60, "expiry": "2021-02-28"},
+        "1": {"name": 1, "price": 0.50, "expiry": "2021-06-30"},
+        "2": {"name": 3, "price": 0.70, "expiry": "2021-02-30"},
+        "3": {"name": 4, "price": 0.80, "expiry": "2021-01-28"},
+        "4": {"name": 5, "price": 0.50, "expiry": "2020-11-30"},
+        "5": {"name": 6, "price": 0.50, "expiry": "2020-10-28"},
+        "6": {"name": 7, "price": 0.50, "expiry": "2020-09-30"},
+        "7": {"name": 8, "price": 0.50, "expiry": "2020-09-28"},
+        "8": {"name": 9, "price": 0.50, "expiry": "2020-09-28"},
     }
     cans = {
         "0": "0",
@@ -20,12 +31,41 @@ def setup_database():
     conn = sqlite3.connect('shop.db')
     conn.execute("PRAGMA foreign_keys = 1")
 
+    c = conn.cursor()
+    c.executescript(open("setup.sql").read())
+    conn.commit()
+
     for name, image in images.items():
+        #print(f"INSERT INTO types (name, image) VALUES ({name}, {image})")
         conn.execute("INSERT INTO types (name, image) VALUES (?, ?)", (name, image))
     for batch in batches.values():
+        #print(f"INSERT INTO batches (type, expiry, price) VALUES ({batch['name']}, {batch['expiry']}, {batch['price']})")
         conn.execute("INSERT INTO batches (type, expiry, price) VALUES (?, ?, ?)", (batch['name'], batch['expiry'], batch['price']))
-    for can in range(3):
-        conn.execute("INSERT INTO cans (batch) VALUES (?)", (can + 1,))
+    x = 1
+    for can in range(x, x+2):  # Dr Peppers x 2
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (1,))
+        x += 1
+    for can in range(x, x+60):  # Full Coke x 60
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (2,))
+        x += 1
+    for can in range(x, x+15):  # Full Pepsi x 15
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (3,))
+        x += 1
+    for can in range(x, x+20):  # Fanta Fruit Twist x 20
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (4,))
+        x += 1
+    for can in range(x, x+24):  # Sprite x 24
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (5,))
+        x += 1
+    for can in range(x, x+27):  # Pepsi Max x 27
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (6,))
+        x += 1
+    for can in range(x, x+9):  # Lilt x 9
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (7,))
+        x += 1
+    for can in range(x, x+15):  # Emerge Energy Drink x 15
+        conn.execute("INSERT INTO cans (batch) VALUES (?)", (8,))
+        x += 1
     conn.commit()
 
     conn.close()
@@ -51,5 +91,5 @@ def main():
 
 # Run the main program
 if __name__ == "__main__":
-    #setup_database()
+    setup_database()
     main()
